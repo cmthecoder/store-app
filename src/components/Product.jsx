86b9064e20d8582b75c124d3eaa58d45406da51e
Chris Mems <christopher.mems@.com>
2023-06-10 13:35:36 -0500

    create the basic layout

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,24 +1,46 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 const Product = () => {
-  const [products, setProducts] = useState([])
-
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     // Call api
-    fetch('https://fakestoreapi.com/products')
-    .then(data => data.json())
-    .then(result => setProducts(result))
-  }, [])
+    fetch("https://fakestoreapi.com/products")
+      .then((data) => data.json())
+      .then((result) => setProducts(result));
+  }, []);
 
-  console.log(products)
+  console.log(products);
+
+  const cards = products.map((product) => (
+    <div key={product.id} className="col-md-3" style={{ marginBottom: "10px" }}>
+      <Card style={{ width: "18rem" }} className="h-100 pt-4">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ width: "100px", height: "130px" }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>Price : ${product.price}</Card.Text>
+        </Card.Body>
+        <Card.Footer style={{ background: 'white'}}>
+          <Button variant="primary">Add to Cart</Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
 
   return (
     <>
-
-    <h1>Product</h1>
+      <h1>Product</h1>
+      <div className="row">{cards}</div>
     </>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
